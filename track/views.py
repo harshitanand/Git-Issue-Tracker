2cbbe2f1c24d49ce2d391ebf4d7fe3fae8226b07
harshitanand <harshitanand94@gmail.com>
2016-10-20 22:52:02 +0530

    Modified logic for issues tracking

--- track/views.py
+++ track/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 import requests as req
 import json
-from datetime import time, date, datetime, timedelta
+import datetime
 # Create your views here.
 
 def result(request):
+
     if request.method == 'GET':
         return render(request, 'index.html')
+    
     if request.method == 'POST':
         url = request.POST['address'].split('/')
         if len(url) == 6 and url[2] == "github.com":
@@ -19,16 +21,14 @@ def result(request):
             total_issues = 0
             lastday_issues = 0
             lastweek_issues = 0
-            for i in range(1,10):
-                res = req.get(path, params={"state":"open", "page":i,"per_page":"100"}).json()
-                total_issues =  total_issues + len(res)
-            current = str(datetime.now()).split(" ")
-            for i in range(1,10):
-                res = req.get(path, params={"state":"open", "page":i,"per_page":"100","since":str(date.today() -timedelta(1)) + "T" + current[1]}).json()
-                lastday_issues = lastday_issues + len(res)
-            for i in xrange(1,10):
-                res = req.get(path, params={"state":"open", "page":i,"per_page":"100","since":str(date.today() -timedelta(7)) + "T" + current[1]}).json()
-                lastweek_issues = lastweek_issues + len(res)
-            return render(request, 'index.html', {'total_issues':total_issues, "lastday_issues":lastday_issues, "lastweek_issues":lastweek_issues, "before_week":total_issues - lastweek_issues})
+            res = req.get(path, params={"state":"open", "direction":"desc"}).json()
+            total_issues =  total_issues + len(res)
+            one_day = datetime.datetime.now() - datetime.timedelta(hours = 24)
+            res = req.get(path, params={"state":"open", "direction": "desc", "since": one_day.strftime('%Y-%m-%d %H:%M:%S')}).json()
+            lastday_issues = lastday_issues + len(res)
+            one_week = one_day - datetime.timedelta(hours = 7*24)
+            res = req.get(path, params={"state":"open", "direction":"desc", "since": one_week.strftime('%Y-%m-%d %H:%M:%S')}).json()
+            lastweek_issues = lastweek_issues + len(res)
+            return render(request, 'index.html', {'total_issues':total_issues, "lastday_issues":lastday_issues, "lastweek_issues":lastweek_issues, "before_week":total_issues - lastweek_issues - lastday_issues})
         else:
-            return render(request, 'index.html', {'error': 'The link git hub repo issues is incorrect it should be like  \'https://github.com/Shippable/support/issues\''})
+            return render(request, 'index.html', {'error': 'Link to GitHub Repository is incorrect it should be like  \'https://github.com/Shippable/support/issues\''})
